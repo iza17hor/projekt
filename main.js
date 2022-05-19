46809f1a8b3c6e9d4f342e3d04013b373a2deb6f
Izabela Horodenska <iza17hor@gmail.com>
2022-05-19 14:06:33 +0200

    modal

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+
 const form = document.getElementById("form");
 const email = document.getElementById("email");
 const vorname = document.getElementById("vorname");
